e232e571f430a8156dfbe3a4ddf81e3e6dbd66e4
Lrome1019 <Laura.romero.lg@gmail.com>
2026-02-07 19:03:28 -0500

    Create Revision and WikiRevisionParse classes in main directory

---
+++ src/main/java/edu/bsu/cs/Revision.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs;
+
+public class Revision {
+    public final String user;
+    public final String timestamp;
+
+
+    public Revision(String user, String timestamp) {
+        this.user = user;
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/edu/bsu/cs/WikiRevisionParse.java
@@ -0,0 +1,10 @@
+package edu.bsu.cs;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class WikiRevisionParse {
+    public List<Revision> parse(InputStream testDataStream) {
+        return null;
+    }
+}
