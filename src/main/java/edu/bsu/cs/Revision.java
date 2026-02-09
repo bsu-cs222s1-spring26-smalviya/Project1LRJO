0d951df4b850a557178a86b3ef71680d9cbadf98
Lrome1019 <Laura.romero.lg@gmail.com>
2026-02-09 11:29:05 -0500

    Added encapsulation to revision class with private getters and setters.

--- src/main/java/edu/bsu/cs/Revision.java
+++ src/main/java/edu/bsu/cs/Revision.java
@@ -1,12 +1,20 @@
 package edu.bsu.cs;
 
 public class Revision {
-    public final String user;
-    public final String timestamp;
+    private final String user;
+    private final String timestamp;
 
 
     public Revision(String user, String timestamp) {
         this.user = user;
         this.timestamp = timestamp;
+    }
+
+    private String getUser() {
+        return user;
+    }
+
+    private String getTimestamp() {
+        return timestamp;
     }
 }
