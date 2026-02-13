e6a4b14aec47a94a7695554e6474b6ecdcc99e7d
Lrome1019 <Laura.romero.lg@gmail.com>
2026-02-13 16:44:08 -0500

    Fixed RevisionFormatter spacing.

--- src/main/java/edu/bsu/cs/RevisionFormatter.java
+++ src/main/java/edu/bsu/cs/RevisionFormatter.java
@@ -9,7 +9,7 @@ public class RevisionFormatter {
      StringBuilder builder = new StringBuilder();
      int count = 1;
      for (Revision revision : revisions) {
-         builder.append(String.format("%d   %s  %s\n",
+         builder.append(String.format("%d  %s  %s\n",
                  count++, revision.getTimestamp(), revision.getUser()));
      }
      return builder.toString();
