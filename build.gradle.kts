2da3e23c20cb03341c7f1b148989870839018fcc
Lrome1019 <Laura.romero.lg@gmail.com>
2026-02-13 17:47:23 -0500

    Add jsonpath dependency to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.jayway.jsonpath:json-path:2.8.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
