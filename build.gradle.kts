8ab1481cafbf50c02aa1510c4e287a25dc3e8cfc
Lrome1019 <Laura.romero.lg@gmail.com>
2026-02-20 20:19:32 -0500

    fix JavaFX plugins and dependencies in gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,26 @@
-
 plugins {
     java
     application
-    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-group = "org.example"
+group = "edu.bsu.cs"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+javafx {
+    version = "17.0.2"
+    modules("javafx.controls", "javafx.fxml")
+}
 dependencies {
     implementation("com.jayway.jsonpath:json-path:2.8.0")
+
+    implementation("org.openjfx:javafx-controls:17.0.2")
+
+    implementation("org.openjfx:javafx-fxml:17.0.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -22,9 +29,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-javafx
-    version= "21"
-    modules("javafx.controls")
-   application {
-    mainClass.set("edu.bsu.cs.GUI")
+application{
+    mainClass.set("edu.bsu.GUI")
+
 }
